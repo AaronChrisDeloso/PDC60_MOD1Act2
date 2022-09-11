ddddceefff46dcc0e93fdd001c0d6b878e4e5574
Aaron Chris Deloso <deloso.aaronchris@auf.edu.ph>
2022-09-11 23:15:47 +0800

    Add project files.

---
+++ PDC60_MOD1Act2/PDC60_MOD1Act2/SharedStatic.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace PDC60_MOD1Act2
+{
+    public class SharedStatic
+    {
+        public static readonly Color ButtonColor = Color.Black;
+        public static readonly Color ButtonTextColor = Color.White;
+
+    }
+}
